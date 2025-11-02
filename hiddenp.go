4f68f5f2b864a0428567ef6d7db4136d29996124
sulubaby <pokefanshere@gmail.com>
2025-11-02 13:34:22 +0300

    Implement hiddenp functionality to check subsequence

---
+++ hiddenp.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		return
+	}
+	s1, s2 := os.Args[1], os.Args[2]
+	i := 0
+	for _, c := range s2 {
+		if i < len(s1) && rune(s1[i]) == c {
+			i++
+		}
+	}
+	if i == len(s1) {
+		z01.PrintRune('1')
+	} else {
+		z01.PrintRune('0')
+	}
+	z01.PrintRune('\n')
+}
